9fb42aa6352fb4d9f3c6dd8b05fe336c27dc6325
Antonio <a.guerrero.esp94@gmail.com>
2020-03-04 22:36:20 -0600

    sigue

--- ordenCompra/model/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class conexion{
-        public static function conn(){
-            $link = mysqli_connect("localhost","root","","proyecto");
-            $link -> set_charset("UTF8");
-            return $link;
-        }
-    }
-?>
